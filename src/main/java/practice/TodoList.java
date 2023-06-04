164ab1ced4519c0d54bc21166ffa6fc3eefa183d
k0na <pwpvpdru@yandex.ru>
2023-06-04 21:10:21 +0300

    lesson_10.9 homework done - realize new methods in TodoList.java - add some logic in Main.java

--- src/main/java/practice/TodoList.java
+++ src/main/java/practice/TodoList.java
@@ -4,28 +4,50 @@ import java.util.ArrayList;
 
 public class TodoList {
 
+    ArrayList<String> list = new ArrayList<>();
+    public final String WARNING = "Дело с таким номером не существует";
+
+    public String getString(int index) {
+        if (isInList(index)){
+            return list.get(index);
+        } else {
+            return WARNING;
+        }
+    }
+
     public void add(String todo) {
-        // TODO: добавьте переданное дело в конец списка
+        list.add(todo);
     }
 
     public void add(int index, String todo) {
-        // TODO: добавьте дело на указаный индекс,
-        //  проверьте возможность добавления
+        if (isInList(index)) {
+            list.add(index, todo);
+        } else {
+            list.add(todo);
+        }
     }
 
     public void edit(int index, String todo) {
-        // TODO: заменить дело на index переданным todo индекс,
-        //  проверьте возможность изменения
+        if (isInList(index)) {
+            list.set(index, todo);
+        } else {
+            System.out.println(WARNING);
+        }
     }
 
     public void delete(int index) {
-        // TODO: удалить дело находящееся по переданному индексу,
-        //  проверьте возможность удаления дела
+        if (isInList(index)) {
+            list.remove(index);
+        } else {
+            System.out.println(WARNING);
+        }
+    }
+
+    private boolean isInList(int index) {
+        return !(index > list.size() - 1) && !(index < 0);
     }
 
     public ArrayList<String> getTodos() {
-        // TODO: вернуть список дел
-        return new ArrayList<>();
+        return list;
     }
-
 }
